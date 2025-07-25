e17da04e3f49aa7978b09b4bf8c8711e185766df
sergiosistemapeoplenet <sergio@sistemapeoplenet.com.br>
2025-07-24 21:22:26 -0300

    Desenvolvimento da edição, exclusão e consulta por ID do cliente

--- CrudSqlServerDapper/Controllers/ClientController.cs
+++ CrudSqlServerDapper/Controllers/ClientController.cs
@@ -25,7 +25,7 @@ namespace CrudSqlServerDapper.Controllers
             Console.WriteLine("(1) CADASTRAR CLIENTE");
             Console.WriteLine("(2) ATUALIZAR CLIENTE");
             Console.WriteLine("(3) EXCLUIR CLIENTE");
-            Console.WriteLine("(4) PESQUISAR CLIENTES");
+            Console.WriteLine("(4) CONSULTAR CLIENTES");
 
             Console.Write("\nINFORME A OPÇÃO DESEJADA...: ");
             var opcao = Console.ReadLine();
@@ -34,6 +34,18 @@ namespace CrudSqlServerDapper.Controllers
             {
                 case "1": //caso a opção seja "1"
                     CreateClient(); //executando o método para cadastrar cliente
+                    break;
+
+                case "2": //caso a opção seja "2"
+                    UpdateClient(); //executando o método para atualizar cliente
+                    break;
+
+                case "3": //caso a opção seja "3"
+                    DeleteClient(); //executando o método para excluir cliente
+                    break;
+
+                case "4": //caso a opção seja "4"
+                    ReadClients(); //executando o método para consultar os clientes
                     break;
 
                 default: //caso não seja nenhum dos anteriores
@@ -96,6 +108,132 @@ namespace CrudSqlServerDapper.Controllers
                     Console.WriteLine($"\tErro: {item.ErrorMessage}");
                 }
             }            
+        }
+
+        /// <summary>
+        /// Método para realizar a atualização do cliente
+        /// </summary>
+        private void UpdateClient()
+        {
+            Console.WriteLine("\nEDIÇÃO DE CLIENTES:\n");
+
+            Console.Write("INFORME O ID DO CLIENTE......: ");
+            var id = Guid.Parse(Console.ReadLine() ?? string.Empty);
+
+            //consultando o cliente no banco de dados através do ID
+            var clientRepository = new ClientRepository();
+            var client = clientRepository.GetById(id);
+
+            //verificar se o cliente foi encontrado
+            if(client != null)
+            {
+                Console.WriteLine("\nDADOS DO CLIENTE:");
+
+                Console.WriteLine($"\tID...........: {client.Id}");
+                Console.WriteLine($"\tNAME.........: {client.Name}");
+                Console.WriteLine($"\tEMAIL........: {client.Email}");
+                Console.WriteLine($"\tBIRTHDATE....: {client.BirthDate}");
+
+                Console.WriteLine("\nINFORME OS DADOS PARA EDIÇÃO:\n");
+
+                Console.Write("INFORME O NOME...............: ");
+                client.Name = Console.ReadLine() ?? string.Empty;
+
+                Console.Write("INFORME O EMAIL..............: ");
+                client.Email = Console.ReadLine() ?? string.Empty;
+
+                Console.Write("INFORME A DATA DE NASCIMENTO.: ");
+                client.BirthDate = DateTime.Parse(Console.ReadLine() ?? string.Empty);
+
+                //Instanciando a classe de validação do cliente
+                var validator = new ClientValidator();
+                //Executar as validações no objeto e capturar o resultado
+                var result = validator.Validate(client);
+
+                //Verificar os dados do cliente passaram nas regras de validação
+                if (result.IsValid)
+                {                    
+                    clientRepository.Update(client); //atualizando o cliente
+
+                    Console.WriteLine("\nCLIENTE ATUALIZADO COM SUCESSO!");
+                }
+                else
+                {
+                    Console.WriteLine("\nOCORRERAM ERROS DE VALIDAÇÃO!");
+                    foreach (var item in result.Errors)
+                    {
+                        Console.WriteLine($"\tErro: {item.ErrorMessage}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nCLIENTE NÃO FOI ENCONTRADO.");
+            }
+        }
+
+        /// <summary>
+        /// Método para realizar a exclusão do cliente
+        /// </summary>
+        private void DeleteClient()
+        {
+            Console.WriteLine("\nEXCLUSÃO DE CLIENTES:\n");
+
+            Console.Write("INFORME O ID DO CLIENTE......: ");
+            var id = Guid.Parse(Console.ReadLine() ?? string.Empty);
+
+            //consultando o cliente no banco de dados através do ID
+            var clientRepository = new ClientRepository();
+            var client = clientRepository.GetById(id);
+
+            //verificar se o cliente foi encontrado
+            if (client != null)
+            {
+                Console.WriteLine("\nDADOS DO CLIENTE:");
+
+                Console.WriteLine($"\tID...........: {client.Id}");
+                Console.WriteLine($"\tNAME.........: {client.Name}");
+                Console.WriteLine($"\tEMAIL........: {client.Email}");
+                Console.WriteLine($"\tBIRTHDATE....: {client.BirthDate}");
+
+                Console.WriteLine("\nDESEJA EXCLUIR O CLIENTE? (S,N): ");
+                var opcao = Console.ReadLine() ?? string.Empty;
+
+                if(opcao.Equals("S", StringComparison.OrdinalIgnoreCase))
+                {
+                    clientRepository.Delete(id);
+
+                    Console.WriteLine("\nCLIENTE EXCLUÍDO COM SUCESSO!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nCLIENTE NÃO FOI ENCONTRADO.");
+            }
+        }
+
+        /// <summary>
+        /// Método para realizar a consulta dos clientes
+        /// </summary>
+        private void ReadClients()
+        {
+            Console.WriteLine("\nCONSULTA DE CLIENTES:\n");
+
+            //Criando um objeto da classe de repositório
+            var clientRepository = new ClientRepository();
+            
+            //consultar e obter uma lista de clientes
+            var clients = clientRepository.GetAll();
+
+            //exibir cada cliente obtido do banco de dados
+            foreach (var item in clients)
+            {
+                Console.WriteLine($"ID..........: {item.Id}");
+                Console.WriteLine($"NAME........: {item.Name}");
+                Console.WriteLine($"EMAIL.......: {item.Email}");
+                Console.WriteLine($"BIRTHDATE...: {item.BirthDate.ToString("dd/MM/yyyy")}");
+                Console.WriteLine("...");
+            }
         }
     }
 }
--- CrudSqlServerDapper/Repositories/ClientRepository.cs
+++ CrudSqlServerDapper/Repositories/ClientRepository.cs
@@ -40,5 +40,87 @@ namespace CrudSqlServerDapper.Repositories
                 connection.Execute(query, client);
             }
         }
+
+        /// <summary>
+        /// Método para atualizar um cliente no banco de dados
+        /// </summary>
+        public void Update(Client client)
+        {
+            //Escrevendo o comando SQL
+            var query = @"
+                UPDATE CLIENT
+                SET
+                    NAME = @Name,
+                    EMAIL = @Email,
+                    BIRTHDATE = @Birthdate
+                WHERE
+                    ID = @Id
+            ";
+
+            //Conectando no banco de dados
+            using (var connection = new SqlConnection(_appSettings.ConnectionString))
+            {
+                connection.Execute(query, client);
+            }
+        }
+
+        /// <summary>
+        /// Método para excluir um cliente no banco de dados
+        /// </summary>
+        public void Delete(Guid id)
+        {
+            //Escrevendo o comando SQL
+            var query = @"
+                DELETE FROM CLIENT
+                WHERE ID = @Id
+            ";
+
+            //Conectando no banco de dados
+            using (var connection = new SqlConnection(_appSettings.ConnectionString))
+            {
+                connection.Execute(query, new { id });
+            }
+        }
+
+        /// <summary>
+        /// Método para retonar uma lista com todos os clientes
+        /// que estão cadastrados na tabela do banco de dados
+        /// </summary>
+        public List<Client> GetAll()
+        {
+            //Escrevendo o comando SQL
+            var query = @"
+                SELECT ID, NAME, EMAIL, BIRTHDATE
+                FROM CLIENT
+                ORDER BY NAME
+            ";
+
+            //Conectando no banco de dados
+            using (var connection = new SqlConnection(_appSettings.ConnectionString))
+            {
+                return connection.Query<Client>(query).ToList();
+            }
+        }
+
+        /// <summary>
+        /// Método para consultar e retornar 1 cliente do banco de dados
+        /// baseado no ID informado ou retornar null se nenhum cliente
+        /// for encontrado.
+        /// </summary>
+        public Client? GetById(Guid id)
+        {
+            //Escrevendo o comando SQL
+            var query = @"
+                SELECT ID, NAME, EMAIL, BIRTHDATE
+                FROM CLIENT
+                WHERE ID = @id
+            ";
+
+            //Conectando no banco de dados
+            using (var connection = new SqlConnection(_appSettings.ConnectionString))
+            {
+                return connection.QueryFirstOrDefault<Client>(query, new { id });
+            }
+        }
     }
 }
